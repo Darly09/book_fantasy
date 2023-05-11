a71cd6cd175f404933b5d3b16ac0ff7d2b1822e4
juan peralta <alter-juan@Alternova.localdomain>
2023-05-10 22:41:25 -0500

    Se arreglan los problemas con los imports

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,6 @@
-<!--Es la configuración inicial del proyecto, donde definimos importaciones, parámetros-->
-<script setup>
-import Home from './views/Home.vue';
-</script>
 <!--Cada template(plantilla), es el componente que reutilizaremos-->
 <template>
   <div>
-   
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
-
