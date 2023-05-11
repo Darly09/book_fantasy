a45f8ab63ba5638777d84f4a623c48789b09b75b
Estefania_villan <evargasvi@cesde.net>
2023-05-10 22:49:06 -0500

    Se crea un nuevo cambio al login

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-
 const routes = [{
     path: '/',
     redirect: 'Home',
@@ -11,7 +8,7 @@ const routes = [{
 {
     path: '/home',
     name: 'Home',
-    component: Home
+    component: () => import('./views/Home.vue')
 },
 {
     path: '/Login',
