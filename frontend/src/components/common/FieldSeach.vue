954605e00bef9ab4082e6a0b0486aae58a3e0087
Darly León <djleonte@cesde.net>
2023-05-16 01:47:14 -0500

    Se modifica el nav bar, de acuerdo al diseño de Nata en Figma, aunque falta el puntero.

--- frontend/src/components/common/FieldSeach.vue
+++ frontend/src/components/common/FieldSeach.vue
@@ -18,20 +18,5 @@ input {
   background-color: #F5F7F9;
   padding-left: 1rem;
   padding-right: 1rem;
-  
-  /*
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-start;
-  padding: 10px 24px;
-  gap: 10px;
-  width: 612px;
-  height: 44px;
-  background: #F5F7F9;
-  border-radius: 10px;
-  flex: none;
-  order: 0;
-  flex-grow: 0;*/
 }
 </style>
--- frontend/src/components/common/NavBar.vue
+++ frontend/src/components/common/NavBar.vue
@@ -2,10 +2,10 @@
     <div>
         <nav>
             <a href="#">Home</a>
-            <a href="#">Book fantasy te recomienda</a>
-            <a href="#">Book fantasy blog</a>
-            <a href="#">Niños</a>
-            <a href="#">Usados</a>
+            <a href="#">Recomendados</a>
+            <a href="#">Libros infantiles</a>
+            <a href="#">Libros usados</a>
+            <a href="#">Blog</a>
             <a href="#">¿Quiénes somos?</a>
         </nav>
     </div>
@@ -13,26 +13,24 @@
 
 <style scoped>
 nav {
-    background-color: #333;
     overflow: hidden;
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-between;
+    justify-content: center;
     padding-left: 2rem;
     padding-right: 1rem;
+    padding-bottom: 2rem;
 }
 
 nav a {
-    color: white;
+    color: #5B656F;
     text-align: center;
-    padding: 1rem 1rem;
+    padding: 1rem 2rem;
     text-decoration: none;
-    flex-grow: 1;
 }
 
 nav a:hover {
-    background-color: #ddd;
-    color: black;
+    color: #D75C37;
 }
 
 </style>
