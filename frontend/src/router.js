01973c9d568c865c5a33af1516f34f84f8874ff3
Carlos10Maya <117688278+Carlos10Maya@users.noreply.github.com>
2023-05-20 17:59:33 -0500

    se crea vista boysbook

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    redirect: "Home",
+    redirect: "Boysbook",
   },
   {
     path: "/home",
@@ -14,6 +14,11 @@ const routes = [
     path: "/Login",
     name: "Login",
     component: () => import("./views/Login.vue"),
+  },
+  {
+    path:"/boysbook",
+    name:"BoysBook",
+    component: () => import("./views/BoysBook.vue"),
   },
 ];
 const router = createRouter({
---
+++ frontend/src/views/BoysBook.vue
@@ -0,0 +1,35 @@
+<script setup>
+import 
+</script>
+
+<template>
+    
+    <section class="header">
+        <h2>Disfrutar y devertirse con un buen libro</h2><br>
+        <h1>LA IMPORTANCIA DE LEER CUENTOS Y LIBROS INFANTILES</h1><br><br><br>
+        <p>¿Te has preguntado por qué es importante leer para los niños y niñas?, ¿sabes cómo enseñarles el hábito de la
+            lectura? El día 2 de abril se celebra el Día Internacional del libro infantil y juvenil, por lo que es una buena
+            ocasión para recordar la importancia de leer cuentos y libros infantiles.</p>
+    </section>
+    <section class="img">
+        <img src="../assets/img/BoysBook/ninos-leen 1.svg" alt="">
+        <div class="imgtext">
+            <p>“La lectura es básica para los niños y niñas porque no solo aporta información, sino que educa, crea buenos
+                hábitos, impulsa a la reflexión, favorece la concentración y hace que disfruten.
+            </p>
+        </div>
+    </section>
+    <section>
+
+        <h1>¿Qué beneficios aporta a los niños y niñas leer cuentos y libros infantiles?</h1>
+        <p>- Los niños y niñas comprenden que pueden aprender por sí mismos y reflexionar sobre lo que aprenden.</p>
+        <p>- Compartir el momento de lectura con nuestros hijos e hijas no solo favorece un hábito saludable, sino que crea un vínculo emocional fuerte con ellos, además de generarse un ambiente de confianza.</p>
+        <p>- Los cuentos y libros infantiles suelen contar historias con las que los niños y niñas se sienten identificados y les ayudan a entender lo que sienten.</p>
+        <p>- La lectura contribuye a que los niños y niñas amplíen su vocabulario, aprendan a expresarse mejor y a escribir correctamente.</p>
+        <p>- Las historias de los cuentos y libros infantiles enseñan valores que ayudarán a los niños y niñas a comprender lo que significa la solidaridad, el respeto, la paz o la igualdad.</p>
+
+    </section>
+</template>
+
+<style></style>
+
