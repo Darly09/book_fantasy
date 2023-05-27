981e68e885be8c8634b55831994bc2156efe5205
felipevelasquez <andres.diaz.velasquez@unillanos.edu.com>
2023-05-26 23:43:02 -0500

    Se resuelve conflictos con la rama de @EstefaniaVillan

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,119 +1,156 @@
 <template>
     <section>
-        <div>
-            <input class="encabezado" id="encabezado" type="text" placeholder="Book Fantasy" disabled>
+        <div class="header">
+            <img src="img/Frame 6.png">
         </div>
 
-        <form class="contenedor">
-            <img class="imagen_perfil" src="../img/usuario.png">
+        <div class="header-book">
+            <img src="img/Frame 2.png">
+        </div>
 
-            <div class="cabecera_correo">
-                <img src="../img/email.png">
-                <label for="FormEmail" class="form-label">Correo </label>
+        <div class="container-form"></div>
+        <div class="img-form">
+        </div>
+        <form class="form">
+            <div class="text-form">
+                <h2>¡Bienvenido!</h2>
+                <p>Para ingresar, completa los campos </p>
             </div>
-            <input type="email" class="form-control" placeholder="Ingrese su correo" id="FormEmail">
-
-            <div class="cabecera_contraseña">
-                <img src="../img/key.png">
-                <label for="Formpassword" class="form-label">Contraseña</label>
+            <div class="input">
+                <label for="user">Email</label>
+                <input placeholder="Ingresa tu nombre" type="text" id="usuario">
             </div>
-            <input type="password" class="form-control" placeholder="Ingrese su contraseña" id="Formpassword">
-
-            <div class="form-check">
-                <label>
-                    ¿Recordar usuario?
-                </label>
-                <input type="checkbox" value="">
+            <div class="input">
+                <label class="password">Contraseña </label>
+                <input placeholder="Ingresa tu contraseña" type="password" id="password">
+                <img src="img/vista.png" onclick="pass()" class="pass-ico">
             </div>
-
             <button type="button" class="button">Ingresar</button>
         </form>
     </section>
 </template>
 
-<style scoped>
-.encabezado {
-    display: flex;
-    background-color: rgb(255, 255, 255);
-    padding: 1rem;
-    width: 50rem;
-    margin: calc(0.5% + 2rem);
-    margin-left: auto;
-    margin-right: auto;
-    border-radius: 1rem;
-    text-align: center;
-    font-size: x-large;
+<script>
+function pass() {
+    var tipo = document.getElementById("password");
+    if (tipo.type == 'password') {
+        tipo.type = 'text';
+    } else {
+        tipo.type = 'password';
+
+    }
 }
 
-section {
-    background-image: url('../img/inicioo.jpg');
-    background-size: cover;
-    background-position: center;
-    padding-top: 10px;
-    height: 60rem;
-    padding-left: 0.5rem;
-}
-.contenedor {
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-    margin: calc(0.5% + 6rem);
-    max-width: max-content;
-    padding: 2rem;
-    margin-left: auto;
-    margin-right: auto;
-    border-radius: 1rem;
-    background-color: #0d58a3a6;
-}
+</script>
 
-.contenedor .imagen_perfil {
-    align-self: center;
-    max-width: 10rem;
-    border-radius: 50%;
-}
+<style scoped> .header {
+     display: flex;
+     flex-direction: row;
+     justify-content: center;
+     align-items: center;
+     padding: 0.5rem;
+     position: absolute;
+     width: 20px;
+     left: 100px;
+     top: 100px;
+ }
 
-.contenedor .cabecera_correo {
-    padding-left: 1rem;
-    color: black;
-    font-size: larger
-}
+ .header-book {
+     display: flex;
+     flex-direction: row;
+     align-items: center;
+     padding: 0.5rem;
+     gap: 8px;
+     position: absolute;
+     width: 174px;
+     height: 28px;
+     left: 102rem;
+     top: 100px;
+ }
 
-.contenedor .cabecera_correo img {
-    max-width: 2rem;
-}
-.contenedor .cabecera_contraseña {
-    padding-left: 0.7rem;
-    color: black;
-    font-size: larger
-}
+ section {
 
-.contenedor .cabecera_contraseña img {
-    max-width: 2rem;
-}
-.contenedor .form-check {
-    display: flex;
-    font-size: larger;
-    padding: 0;
-}
-.contenedor .form-control {
-    border-radius: 1rem;
-}
-.contenedor .form-check input {
-    max-width: 1rem;
-}
-input {
-    width: 20rem;
-}
-.button {
-    border: none;
-    border-radius: 0.9rem;
-    padding: 0.5rem 5rem;
-}
-button:active {
-    transform: translateY(4px);
-}
-button:hover {
-    background-color: #748da5a6;
-}
-</style>
+     display: flex;
+     align-items: center;
+     height: 90vh;
+     position: fixed;
+     top: 0;
+     left: 0;
+ }
 
+ .img-form {
+     flex-direction: row;
+     align-items: center;
+     flex: 0 0 calc(50%);
+     margin: calc(65% + 25px);
+     background-image: url('img/Login.png');
+     border-radius: 5%;
+     background-size: cover;
+     margin-top: 27rem;
+     position: absolute;
+     width: 398px;
+     height: 395px;
+ }
+
+ @media(min-width:768px) {
+     .container-form {
+         display: flex;
+     }
+ }
+
+ .form {
+     padding: 30rem;
+     background-color: #ffffff;
+
+ }
+
+ .text-form h2 {
+     font-size: 40px;
+     text-align: center;
+     font-size: 30px;
+ }
+
+ .text-form p {
+     font-size: 20px;
+     text-align: center;
+ }
+
+ .input label {
+     display: block;
+     font-size: 20px;
+ }
+
+ .input .pass-ico {
+     position: absolute;
+     right: 28rem;
+     padding: 4.5rem;
+     width: 25px;
+     height: 25px;
+     top: 50%;
+     cursor: pointer;
+ }
+
+ .input input {
+     background-color: #F5F7F9;
+     margin-bottom: 10px;
+     width: 15rem;
+     padding: 10px 10px;
+     border-radius: 10px;
+     border: 1px solid transparent;
+ }
+
+ .button {
+     background-color: #D75C37;
+     border: 1px solid transparent;
+     width: 16rem;
+     padding: 10px;
+     border-radius: 10px;
+ }
+
+ .button:active {
+     transform: translateY(4px);
+ }
+
+ .button:hover {
+     background-color: #e46444a6;
+ }</style>
