c3bb885fe23e1a75308095d0eb4cb88f3a8763e1
alejandro2796 <Alejosd27@gmail.com>
2023-05-27 21:56:51 -0500

    en este nuego guardado informo que hubo cambio en el GetBooksViews.js con un try catch

--- backend/views/books/GetBooksViews.js
+++ backend/views/books/GetBooksViews.js
@@ -2,7 +2,13 @@ const express = require("express")
 const getBooks = require("../../controller/books/GetBooksController")
 const router = express.Router()
 
+
 router.get("/",async (request,response )=>{
-    response.send (await getBooks())
+     try{
+        const books = await getBooks();
+        response.send (books)
+       } catch (error) {
+        response.send ("lo siento no se pueden obtener los libros", error)
+    }
 })
 module.exports = router
