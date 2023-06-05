bf2cacb392e874a437a882738ecea52b13e0fa64
alejandro2796 <Alejosd27@gmail.com>
2023-06-05 16:43:01 -0500

    correción en el codigo de inportaciín en el controller del book

--- backend/controller/books/index.js
+++ backend/controller/books/index.js
@@ -2,7 +2,7 @@ const { postCreateController } = require("./CreateBooksController");
 const { deleteBookController } = require("./DeleteBooksController");
 const { getBookById } = require("./GetBookByIdController");
 const { updateBook } = require("./PutBooksController");
-const getBooks = require("./GetBooksController");
+const {getBooks} = require("./GetBooksController");
 const { getBookGender } = require("./Generobookcontroller");
 
 module.exports = {
